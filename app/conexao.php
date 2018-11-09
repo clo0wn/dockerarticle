cdc2da44eb39f39ba1ea8776491fc1f6cb502e0e
clo0wn <44882409+clo0wn@users.noreply.github.com>
2018-11-09 17:18:53 -0200

    Update conexao.php

--- app/conexao.php
+++ app/conexao.php
@@ -3,6 +3,20 @@ define('HOST', '127.0.0.1');
 define('USUARIO', 'root');
 define('SENHA', '123.456');
 define('DB', 'db');
+
+function db_connect()
+{
+    $pdo = new PDO("mysql:host=mysql;dbname=db", 'root', '123.456');
+    return $PDO;
+}
+
+function isLoggedIn()
+{
+    if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true)
+    {
+        return false;
+    }
  
-$pdo = new PDO("mysql:host=mysql;dbname=db", 'root', '123.456');
+    return true;
+}
 ?>
