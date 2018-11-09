74fd0ada777d9d2e7acff132fc19951f22f02d59
clo0wn <44882409+clo0wn@users.noreply.github.com>
2018-11-09 11:41:30 -0200

    Update login.php

--- app/login.php
+++ app/login.php
@@ -1,36 +1,27 @@
-<?php 
-
-define('HOST', '127.0.0.1');
-define('USUARIO', 'root');
-define('SENHA', '123.456');
-define('DB', 'db');
-$login = $_POST['login'];
-$senha = MD5($_POST['senha']);
-$connect = mysqli_connect(HOST, USUARIO, SENHA, DB);
-$db = mysql_select_db(DB);
-$query_select = "SELECT login FROM cadastro WHERE login = '$login'";
-$select = mysql_query($query_select,$connect);
-$array = mysql_fetch_array($select);
-$logarray = $array['login'];
+<?php
+session_start();
+include('conexao.php');
  
-  if($login == "" || $login == null){
-    echo"<script language='javascript' type='text/javascript'>alert('O campo login deve ser preenchido');window.location.href='cadastro.html';</script>";
+if(empty($_POST['usuario']) || empty($_POST['senha'])) {
+	header('Location: index.php');
+	exit();
+}
  
-    }else{
-      if($logarray == $login){
+$usuario = mysqli_real_escape_string($conexao, $_POST['usuario']);
+$senha = mysqli_real_escape_string($conexao, $_POST['senha']);
  
-        echo"<script language='javascript' type='text/javascript'>alert('Esse login já existe');window.location.href='cadastro.html';</script>";
-        die();
+$query = "select usuario from cadastro where usuario = '{$usuario}' and senha = md5('{$senha}')";
  
-      }else{
-        $query = "INSERT INTO cadastro (login,senha) VALUES ('$login','$senha')";
-        $insert = mysql_query($query,$connect);
-         
-        if($insert){
-          echo"<script language='javascript' type='text/javascript'>alert('Usuário cadastrado com sucesso!');window.location.href='login.html'</script>";
-        }else{
-          echo"<script language='javascript' type='text/javascript'>alert('Não foi possível cadastrar esse usuário');window.location.href='cadastro.html'</script>";
-        }
-      }
-    }
-?>
+$result = mysqli_query($conexao, $query);
+ 
+$row = mysqli_num_rows($result);
+ 
+if($row == 1) {
+	$_SESSION['usuario'] = $usuario;
+	header('Location: painel.php');
+	exit();
+} else {
+	$_SESSION['nao_autenticado'] = true;
+	header('Location: index.php');
+	exit();
+}
