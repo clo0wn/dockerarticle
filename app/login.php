f0b265dd1b3850e7c382e289d99d307ffed5b762
clo0wn <44882409+clo0wn@users.noreply.github.com>
2018-11-09 16:02:42 -0200

    Update login.php

--- app/login.php
+++ app/login.php
@@ -5,7 +5,8 @@ include('conexao.php');
 if(empty($_POST['usuario']) || empty($_POST['senha'])) {
 	header('Location: index.php');
 	exit();
-	
+}
+
 $usuario = mysqli_real_escape_string($conexao, $_POST['usuario']);
 $senha = mysqli_real_escape_string($conexao, $_POST['senha']);
  
