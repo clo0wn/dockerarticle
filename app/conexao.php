06247e8148c22fcd5601a5ab51bacfd62cbeedad
clo0wn <44882409+clo0wn@users.noreply.github.com>
2018-11-09 11:36:26 -0200

    Update conexao.php

--- app/conexao.php
+++ app/conexao.php
@@ -1,26 +1,7 @@
 <?php
-  
-class Conexao
-{
-   private static $connection;
-  
-   private function __construct(){}
-  
-   public static function getConnection() {
-  
-       $pdoConfig  = sqlsrv . ":". "Server=" . 127.0.0.1 . ";";
-       $pdoConfig .= "Database=".db.";";
-       
-       try {
-           if(!isset($connection)){
-               $connection =  new PDO($pdoConfig, root, 123.456);
-               $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-           }
-           return $connection;
-       } catch (PDOException $e) {
-           $mensagem = "Drivers disponiveis: " . implode(",", PDO::getAvailableDrivers());
-           $mensagem .= "\nErro: " . $e->getMessage();
-           throw new Exception($mensagem);
-       }
-   }
-}
+define('HOST', '127.0.0.1');
+define('USUARIO', 'root');
+define('SENHA', '123.456');
+define('DB', 'db');
+ 
+$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
