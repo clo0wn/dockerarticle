11bf99bbafed8d3f63d2e5b835760ebdeb3154c3
clo0wn <44882409+clo0wn@users.noreply.github.com>
2018-11-09 17:42:36 -0200

    Update cadastro.php

--- app/cadastro.php
+++ app/cadastro.php
@@ -6,7 +6,7 @@ if(empty($_POST['usuario']) || empty($_POST['senha'])) {
 	header('Location: index.php');
 	exit();
 }
-
+$pdo = new PDO("mysql:host=mysql;dbname=db", 'root', '123.456');
 $stmt = $pdo->prepare('INSERT INTO cadastro (usuario, senha) VALUES(:usuario, :senha)');
 $stmt->execute(array(':usuario' => $usuario, ':senha' => $senha,));
 HEADER('LOCATION: login.html');
