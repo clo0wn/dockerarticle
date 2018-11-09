16b4d5bc5570ee9a1c8f5028401a258c2282beaf
clo0wn <44882409+clo0wn@users.noreply.github.com>
2018-11-09 15:39:04 -0200

    Update login.php

--- app/login.php
+++ app/login.php
@@ -5,12 +5,11 @@ include('conexao.php');
 if(empty($_POST['usuario']) || empty($_POST['senha'])) {
 	header('Location: index.php');
 	exit();
-}
- 
+	
 $usuario = mysqli_real_escape_string($conexao, $_POST['usuario']);
 $senha = mysqli_real_escape_string($conexao, $_POST['senha']);
  
-$query = "select usuario from cadastro where usuario = '{$usuario}' and senha = md5('{$senha}')";
+$query = "select usuario from usuario where usuario = '{$usuario}' and senha = '{$senha}'";
  
 $result = mysqli_query($conexao, $query);
  
@@ -25,3 +24,4 @@ if($row == 1) {
 	header('Location: index.php');
 	exit();
 }
+?>
