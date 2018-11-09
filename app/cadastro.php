c593b1f24b05482065994293bd7d97e10fe263e7
clo0wn <44882409+clo0wn@users.noreply.github.com>
2018-11-09 14:57:13 -0200

    Update cadastro.php

--- app/cadastro.php
+++ app/cadastro.php
@@ -1,29 +1,13 @@
 <?php
 session_start();
 include('conexao.php');
- 
+
 if(empty($_POST['usuario']) || empty($_POST['senha'])) {
 	header('Location: index.php');
 	exit();
 }
- 
-$usuario = mysqli_real_escape_string($conexao, $_POST['usuario']);
-$senha = mysqli_real_escape_string($conexao, $_POST['senha']);
- 
-$query = "INSERT INTO usuarios (login,senha) VALUES ('$login','$senha')";
-$insert = mysql_query($query,$conexao);
- 
-$result = mysqli_query($conexao, $query);
- 
-$row = mysqli_num_rows($result);
- 
-if($row == 1) {
-	$_SESSION['usuario'] = $usuario;
-	header('Location: painel.php');
-	exit();
-} else {
-	$_SESSION['nao_autenticado'] = true;
-	header('Location: index.php');
-	exit();
-}
-?>
+
+$stmt = $pdo->prepare('INSERT INTO cadastro (usuario, senha) VALUES(:usuario, :senha)');
+$stmt->execute(array(':usuario' => $usuario, ':senha' => $senha,));
+HEADER('LOCATION: login.php');
+:>
